b9ed170a072960b8dc25fe73583d6850af68ae4a
phl0 <florian@florian-wolters.de>
2016-12-22 23:11:10 +0100

    Add first version of working code

---
+++ 2820gpssim.c
@@ -0,0 +1,47 @@
+/*
+ * 2820gpssim
+ * Fixed GPS Position Simulator for IC-2820
+ *
+ * Created: 22.12.2016
+ *
+ */
+
+#define F_CPU 8000000UL
+#define BAUDRATE 9600
+#define MYUBBR ((F_CPU / (BAUDRATE * 16L)) - 1)
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+
+void uart_puts(const char *s) {
+   while (*s) {
+      while (!(UCSRA & (1<<UDRE)));
+      UDR = *s;
+      s++;
+   }
+}
+
+int main (void) { 
+
+   char APRSstring[50];
+   strcpy(APRSstring, "$$CRC843B,DF2ET>API282,DSTAR*:!5129.64N/00714.43Ey/Florian N18 Bochum\r");
+
+   UBRRH = (uint8_t)(MYUBBR >> 8);
+   UBRRL = (uint8_t)(MYUBBR);
+
+   UCSRB = (1<<TXEN);
+
+   UCSRC = (1 << USBS) | (3 << UCSZ0);
+
+   DDRD |= _BV(PD5);
+
+   for (;;) {
+      PORTD |= _BV(PD5);
+      uart_puts(APRSstring);
+      PORTD &=~ _BV(PD5);
+      _delay_ms(3000);
+   }
+   return 0;
+
+}
